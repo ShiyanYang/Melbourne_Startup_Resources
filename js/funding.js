60dab617e6d991efd283af52423e863a5da85228
fun_duck <dgysy501@gmail.com>
2025-06-25 04:39:42 +0000

    modify information

--- js/funding.js
+++ js/funding.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
             name: 'LaunchVIC',
             link: 'https://launchvic.org/',
             description: "Victoria Government's independent startup agency supporting founders and investors",
-            category:'Pre-seed, seed, and series A',
+            category:'Pre-seed, seed',
             domain: 'General Industry, Ecosystem Support',
             amount: 'Up to $50,000 equity-free funding and other blended capital',
             grant_link: 'https://launchvic.org/grants/#open',
@@ -37,40 +37,13 @@ document.addEventListener('DOMContentLoaded', function() {
             name: 'Melbourne Accelerator Program (MAP)',
             link: 'https://www.themap.co/',
             description: "The University of Melbourne's startup accelerator",
-            category:'Seed',
-            domain: 'General Industry',
+            category:'Pre-seed, seed',
+            domain: 'Various industries',
             amount: '$20,000 equity-free funding',
             metric: 'Book office hours for founders',
             metric_link: "https://www.themap.co/apply"
         },
-
-        {
-            name: 'Stone & Chalk',
-            link: 'https://www.stoneandchalk.com.au/',
-            description: "The largest innovation community in Australia, supporting startups and scaleups",
-            category:'',
-            domain: 'General Industry',
-            amount: '$1b capital raised by residents and alumni',
-            metric: 'Join residency program',
-            metric_link: 'https://www.stoneandchalk.com.au/membership'
-        },
-         
-    ];
-
-    const funding_seed_to_A = [
-        {
-            name: 'Skalata Ventures',
-            link: 'https://www.skalata.vc/',
-            description: "Help pre-seed founders build the capabilities for long term success",
-            category:'Pre-seed',
-            domain: 'General Industry',
-            amount: 'Up to $300,000, with $500k+$1m+$2m follow-on',
-            amount_link: 'https://www.skalata.vc/founders/get-investment',
-            metric: 'Founder application',
-            metric_link: 'https://www.skalata.vc/contact'
-        },
-     
-        {
+     {
             name: 'Startmate',
             link: 'https://startmate.com/',
             description: "A leading Australian accelerator investing 230+ startups across various sectors",
@@ -81,12 +54,62 @@ document.addEventListener('DOMContentLoaded', function() {
             metric: '2026 Startmate Accelerator Program Register (12 weeks)',
             metric_link: 'https://www.startmate.com/accelerator?utm_source=website&utm_medium=link&utm_campaign=AccW25-website_navigation&utm_content=homepage_hero'
         },
+
+        {
+            name: 'Startupbootcamp',
+            link: 'https://www.startupbootcamp.org/',
+            description: "Global network of industry-focused startup accelerators",
+            category:'Seed, Series A',
+            domain: 'Various industries',
+            amount: '€1.7m average funding',
+            metric: 'Join 3-month accelerator program',
+            metric_link: 'https://www.startupbootcamp.org/startups/accelerators'
+        },
+
+         {
+            name: 'MedTech Actuator',
+            link: 'https://medtechactuator.com/',
+            description: "Australia's first dedicated medtech accelerator, supporting startups in the healthcare sector",
+            category:'Seed, Series A',
+            domain: 'MedTech, HealthTech, BioTech',
+            amount: '$62m+ raised by startups',
+            metric: 'Join 9-month accleration program (application open Feburary - March)',
+            metric_link: 'https://medtechactuator.com/accelerator/'
+        },
+
+         {
+            name: 'ANDHealth',
+            link: 'https://www.andhealth.com.au/',
+            description: "Leading digital health commercialisation program fundedd by the Australian Government",
+            category:'Seed',
+            domain: 'Digital Health',
+            amount: '$1.1m raised on average 6 months post-program',
+            metric: 'Book office hours for founders',
+            metric_link: 'https://www.andhealth.com.au/our-programs/office-hours'
+        },
+         
+    ];
+
+    const funding_seed_to_A = [
+        {
+            name: 'Skalata Ventures',
+            link: 'https://www.skalata.vc/',
+            description: "Help pre-seed founders build the capabilities for long term success",
+            category:'Pre-seed',
+            domain: 'Various industries',
+            amount: 'Up to $300,000, with $500k+$1m+$2m follow-on',
+            amount_link: 'https://www.skalata.vc/founders/get-investment',
+            metric: 'Founder application',
+            metric_link: 'https://www.skalata.vc/contact'
+        },
+     
+        
         {
             name: 'Antler',
             link: 'https://www.antler.co/residency/australia',
             description: "Global startup generator and early-stage venture capital firm",
             category:'Pre-seed, seed, series A',
-            domain: 'General Industry',
+            domain: 'Various industries',
             amount: '$260,000 pre-Seed funding (12% equity stake, $75,000 platform fee)',
             metric: '10-week rediency program',
             metric_link: 'https://www.antler.co/apply#location'
@@ -96,7 +119,7 @@ document.addEventListener('DOMContentLoaded', function() {
             name: 'Boab Ventures',
             link: 'https://boab.ventures/',
             description: "A subsidiary of Artesian Venture Partners, focusing on deeptech startups",
-            category:'Seed',
+            category:'Seed, Sereis A',
             domain: 'Deeptech',
             amount: '$300,000-$1.6m in first round, with up to $3m follow-on',
             metric: 'Open programs for application',
@@ -107,7 +130,7 @@ document.addEventListener('DOMContentLoaded', function() {
             name: 'Rocket Seeder',
             link: 'https://www.rocketseeder.com/',
             description: "Supports early-stage startups aligned to the UN's SDGs",
-            category:'Pre-seed, seed',
+            category:'Seed',
             domain: 'AgTech',
             amount: 'Funding unknown',
             metric: 'AgTech Seeds Program',
@@ -118,20 +141,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const others = [
         {
-            name: 'Australian Government Grants',
+            name: 'Australian Government Current Grant Opportunities',
             link: 'https://help.grants.gov.au//getting-started-with-grantconnect/information-made-easy/current-opportunities/',
-            description: 'Grants to support Australia’s jobs, growth and innovation',
+            description: 'Grants to support Australia’s jobs, growth and innovation across various sectors',
             category:'Government Grant',
-            domain: 'General Industry',
-            amount: 'Varies'
+            domain: 'Various sectors',
+            amount: '$ varies',
+            metrics: ''
         },
 
         {
             name: 'Victorian Government Grants and Programs',
             link: 'https://www.vic.gov.au/grants-and-programs',
-            description: 'Supports businesses in Victoria',
+            description: 'Supports wellbeing in Victoria',
             category:'Government Grant',
-            domain: 'General Industry',
+            domain: 'Various sectors',
             amount: '$ varies'
             
         },
@@ -141,7 +165,7 @@ document.addEventListener('DOMContentLoaded', function() {
             link: 'https://business.vic.gov.au/grants-and-programs?filter=%7B%22status%22%3A%5B%22opening+soon%22%2C%22open%22%2C%22ongoing%22%5D%7D&page=1',
             description: 'Find various grants and programs to support Victorian businesses',
             category:'Government Grant',
-            domain: 'General Industry',
+            domain: 'Various industries',
             amount: '$ varies'
         },
   
@@ -149,8 +173,8 @@ document.addEventListener('DOMContentLoaded', function() {
             name: 'Research and Development Tax Incentive',
             link: 'https://www.industry.gov.au/science-technology-and-innovation/industry-innovation/research-and-development-tax-incentive',
             description: 'Encouraging Australian businesses to invest in research and development',
-            category:'Government Grant',
-            domain: 'General Industry',
+            category:'Government Incentive',
+            domain: 'Various industries',
             amount: '$ varies'
         }
     ];
@@ -191,10 +215,9 @@ document.addEventListener('DOMContentLoaded', function() {
         });
         return sectionHtml;
     }
-
-    html += renderSection('Government, University & Community', funding_seed);
-    html += renderSection('Venture Capital', funding_seed_to_A);
-    html += renderSection('Other Public Grants', others);
+    html += renderSection('Government-Supported Programs and Accelerators', funding_seed);
+    html += renderSection('Venture Capitals', funding_seed_to_A);
+    html += renderSection('Other Government Grants', others);
 
     container.innerHTML = html;
 });
